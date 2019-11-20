dbeb654286a99d1bf5e249fa6c1c8c0c7146f497
David Cornish <davidcornish@Davids-MacBook-Pro.local>
2019-11-20 15:04:55 -0600

    bank exercise added

--- Program.cs
+++ Program.cs
@@ -182,7 +182,35 @@ namespace linq
             {
                 Console.WriteLine(item);
             }
+            List<Customer> customers = new List<Customer>() {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+        };
+            var groups = customers.Where(c => c.Balance >= 1_000_000)
+                        .GroupBy(c => c.Bank);
 
+            foreach (var group in groups)
+            {
+                Console.WriteLine(group.Key);
+                foreach (Customer customer in group)
+                {
+                    Console.WriteLine($"**** {customer.Name}");
+                }
+            }
         }
+    }
+    public class Customer
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
     }
 }

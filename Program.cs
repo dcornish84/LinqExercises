c5f3c73ca559986e22b064caf19587d2c1238cbf
David Cornish <davidcornish@Davids-MacBook-Pro.local>
2019-11-20 14:52:02 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            List<string> LFruits = fruits.Where(fruit =>
+            {
+                bool L = fruit.StartsWith("L");
+                return L;
+            }).ToList();
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            List<int> fourSixMultiples = numbers.Where(num =>
+            {
+                bool isMultiples = (num % 4 == 0 || num % 6 == 0);
+                return isMultiples;
+            }).ToList();
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather",
+                "James",
+                "Xavier",
+                "Michelle",
+                "Brian",
+                "Nina",
+                "Kathleen",
+                "Sophia",
+                "Amir",
+                "Douglas",
+                "Zarley",
+                "Beatrice",
+                "Theodora",
+                "William",
+                "Svetlana",
+                "Charisse",
+                "Yolanda",
+                "Gregorio",
+                "Jean-Paul",
+                "Evangelina",
+                "Viktor",
+                "Jacqueline",
+                "Francisco",
+                "Tre"
+            };
+
+            List<string> descend = names.OrderByDescending(name => name).ToList();
+
+            // Build a collection of these numbers sorted in ascending order
+            List<int> numbersAscend = new List<int>()
+            {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            List<int> Ascend = numbersAscend.OrderBy(numbersAsc => numbersAsc).ToList();
+
+            // Output how many numbers are in this list
+            List<int> fakeChickens = new List<int>()
+            {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            int count = fakeChickens.Count();
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29,
+                745.31,
+                21.76,
+                34.03,
+                4786.45,
+                879.45,
+                9442.85,
+                2454.63,
+                45.65
+            };
+
+            double presents = purchases.Sum();
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45,
+                9442.85,
+                2454.63,
+                45.65,
+                2340.29,
+                34.03,
+                4786.45,
+                745.31,
+                21.76
+            };
+
+            double total = prices.Max();
+
+            /*
+    Store each number in the following List until a perfect square
+    is detected.
+
+    Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+            */
+            List<int> wheresSquaredo = new List<int>()
+            {
+                66,
+                12,
+                8,
+                27,
+                82,
+                34,
+                7,
+                50,
+                19,
+                46,
+                81,
+                23,
+                30,
+                4,
+                68,
+                14
+            };
+            var result = wheresSquaredo.TakeWhile(num =>
+            {
+                bool isSquared = Math.Sqrt(num) % 1 == 0;
+                return !isSquared;
+            }).ToList();
+
+            foreach (int item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
